53c4ed66b73bbe30f690793779ef193eb79fa0ec
Vladimir Kosolapov <v.kosolapov@hotmail.com>
2022-06-17 15:31:55 +0300

    Detection model

--- anchor_free/main.py
+++ anchor_free/main.py
@@ -2,15 +2,15 @@ from pytorch_lightning import Trainer, seed_everything
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from pytorch_lightning.loggers import TensorBoardLogger
 
-from model.classification_model import ClassificationModel
-from data.classification_data import ClassificationDataModule
+from model.detection_model import DetectionModel
+from data.detection_data import DetectionDataModule
 from consts import *
 
 
 def main():
     seed_everything(seed=42, workers=True)
-    model = ClassificationModel()
-    data = ClassificationDataModule()
+    model = DetectionModel()
+    data = DetectionDataModule()
     trainer = Trainer(
         accelerator=TRAINER_ACCELERATOR,
         devices=TRAINER_DEVICES,
