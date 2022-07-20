f06bf76cbe2e4ff62a904a2988240c75eb2f9f25
vkosolapov <v.kosolapov@hotmail.com>
2022-07-20 09:06:54 +0200

    Change UNet

--- anchor_free/model/unet.py
+++ anchor_free/model/unet.py
@@ -83,15 +83,15 @@ class OutConv(nn.Module):
 class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels, mid_channels=None):
         super().__init__()
-        if not mid_channels:
-            mid_channels = out_channels
+        # if not mid_channels:
+        mid_channels = out_channels
         self.double_conv = nn.Sequential(
             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(mid_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
+            # nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            # nn.BatchNorm2d(out_channels),
+            # nn.ReLU(inplace=True),
         )
 
     def forward(self, x):
