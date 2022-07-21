38127537ec2f809850b66e76df1f168ea342f23f
vkosolapov <v.kosolapov@hotmail.com>
2022-07-21 13:24:33 +0200

    Test segmentation

--- anchor_free/main.py
+++ anchor_free/main.py
@@ -2,15 +2,15 @@ from pytorch_lightning import Trainer, seed_everything
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from pytorch_lightning.loggers.wandb import WandbLogger
 
-from model.detection_model import DetectionModel
-from data.detection_data import DetectionDataModule
+from model.segmentation_model import SegmentationModel
+from data.segmentation_data import SegmentationDataModule
 from consts import *
 
 
 def main():
     seed_everything(seed=42)
-    model = DetectionModel()
-    data = DetectionDataModule()
+    model = SegmentationModel()
+    data = SegmentationDataModule()
     trainer = Trainer(
         accelerator=TRAINER_ACCELERATOR,
         devices=TRAINER_DEVICES,
