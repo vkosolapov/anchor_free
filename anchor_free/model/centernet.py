276ff7ea16c153cd0a7a65706930a59a762a1d69
vkosolapov <v.kosolapov@hotmail.com>
2022-07-27 10:46:03 +0200

    Add separate module for losses

--- anchor_free/model/centernet.py
+++ anchor_free/model/centernet.py
@@ -4,25 +4,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision.ops import sigmoid_focal_loss, nms
 
+from model.loss import RegressionLossWithMask
 from consts import *
-
-
-class RegressionLossWithMask(nn.Module):
-    def __init__(self, smooth=False):
-        super().__init__()
-        self.smooth = smooth
-
-    def forward(self, pred, target, mask):
-        pred = pred.permute(0, 2, 3, 1)
-        target = target.permute(0, 2, 3, 1)
-        expand_mask = torch.unsqueeze(mask, -1).repeat(1, 1, 1, 2)
-        if self.smooth:
-            loss_func = F.smooth_l1_loss
-        else:
-            loss_func = F.l1_loss
-        loss = loss_func(pred * expand_mask, target * expand_mask, reduction="sum")
-        loss = loss / (expand_mask.sum() + 1e-4)
-        return loss
 
 
 class CenterNet(nn.Module):
