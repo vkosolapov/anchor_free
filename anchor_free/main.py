7e56a0ef3c447cd2a00510da1353d1b142b2cc9d
vkosolapov <v.kosolapov@hotmail.com>
2022-08-30 11:59:26 +0200

    Detection with sigmoid in postprocessing

--- anchor_free/main.py
+++ anchor_free/main.py
@@ -7,15 +7,15 @@ from pytorch_lightning.callbacks import (
 )
 from pytorch_lightning.loggers.wandb import WandbLogger
 
-from model.classification_model import ClassificationModel
-from data.classification_data import ClassificationDataModule
+from model.detection_model import DetectionModel
+from data.detection_data import DetectionDataModule
 from consts import *
 
 
 def main():
     seed_everything(seed=42, workers=True)
-    model = ClassificationModel()
-    data = ClassificationDataModule()
+    model = DetectionModel()
+    data = DetectionDataModule()
     trainer = Trainer(
         accelerator=TRAINER_ACCELERATOR,
         devices=TRAINER_DEVICES,
