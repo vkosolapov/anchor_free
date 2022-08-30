451d6d5176f6652a4eaf61daedbb458bc1720c91
vkosolapov <v.kosolapov@hotmail.com>
2022-08-30 13:13:33 +0200

    Debug of boxes count logging

--- anchor_free/model/detection_model.py
+++ anchor_free/model/detection_model.py
@@ -141,6 +141,7 @@ class DetectionModel(AbstractModel):
         logits = self.forward(x)
         predictions = self.head.postprocess_predictions(logits)
         boxes_count = sum([pred.shape[0] for pred in predictions])
+        print(boxes_count)
         self.log(
             f"boxes/{phase}",
             boxes_count,
