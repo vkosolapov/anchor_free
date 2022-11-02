9d7eb66ff07697d9517701f1b98496895d481392
vkosolapov <v.kosolapov@hotmail.com>
2022-11-02 15:07:14 +0100

    Fix FPN

--- anchor_free/model/fpn.py
+++ anchor_free/model/fpn.py
@@ -33,9 +33,8 @@ class FPN(nn.Module):
         inner = self.inner_convs[-1](inputs[-1])
         outs = [self.out_convs[-1](inner)]
 
-        print(inputs.size())
         for feat, inner_conv, out_conv in zip(
-            inputs[:-1][::-1], self.inner_convs[:-1][::-1], self.out_convs[:-1][::-1]
+            inputs[:-1].flip(0), self.inner_convs[:-1][::-1], self.out_convs[:-1][::-1]
         ):
             if inner_conv is None:
                 continue
